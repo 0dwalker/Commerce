14f752426c6cf3504969b8414280e04d35c8bded
0dwalker <31480998+0dwalker@users.noreply.github.com>
2018-11-16 13:57:35 -0600

    Add files via upload

--- commerce-incomplete.ts
+++ commerce-incomplete.ts
@@ -3,24 +3,123 @@ class Bank{
     accountName:string[];
     total:number[];
     accountPinn:string[];
-    verifyAccount()
-    verifyAccess()
-    deposit()
-    withdraw()
-}//end of Bank class
+
+    constructor(aid,an,t,ap){
+        this.accountID=aid;
+        this.accountName=an;
+        this.total=t;
+        this.accountPinn=ap;
+    };
+
+    verifyAccount(accountID:string):boolean {
+        if(this.accountID.indexOf(accountID)!= -1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    };
+
+    verifyAccess(accountPinn:string):boolean {
+        if(this.accountPinn.indexOf(accountPinn)!= -1){
+            return true;
+        }
+        else{
+            return false;
+    deposit(Deposemoney:number, accountID:string, total:number){
+        if(Bank.verifyAccount == true){
+            total = Deposemoney + total;
+        }
+        else{
+            console.log("go home goof");
+        }
+    };
+
+    withdraw(withdraw:number, accountID:string, total:number){
+        if(Bank.verifyAccount == true & Bank.verifyAccess == true){
+            total = total - withdraw;
+        }
+        else{
+            console.log("go home goof ")
+        }
+    };
+}
+//end of Bank class
+
+
+
+
+
+
+
+
+
+
+
 class Customer{
-    accountID:string[];
-    accountName:string[];
-    total:number[];
-    accountPinn:string[];
-}//end of Customer class
+    accountID:string;
+    accountName:string;
+    total:number;
+    accountPinn:string;
+
+    constructor(aid,an,t,ap){
+        this.accountID = aid;
+        this.accountName = an;
+        this.total = t;
+        this.accountPinn = ap;
+    };
+}
+//end of Customer class
+
+
+
+
+
+
+
+
+
 class Transaction{
     FromAccountID:string
     pinn:string
     ToAccountID:string
     transType:string
     amount:number
-}//end of Transaction class
+
+    constructor(paid,p,taid,tt,a){
+        this.FromAccountID = paid;
+        this.pinn = p;
+        this.ToAccountID = taid;
+        this.transType = tt;
+        this.amount = a;
+    };
+}
+//end of Transaction class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //creating the tests
 function main () : void{
 
@@ -29,7 +128,7 @@ let charliedata = ["12315673","charbell1231",2.37,"7133"];
 const Charlie = new Customer(...charliedata);
 
 let C = ["34659678","dillydilly",47.53,"8255"];
-const Dillion = new Customer(...dilliondata);
+const Dillon = new Customer(...dillondata);
 
 let delanodata = ["34567655",delwalk,97.45,"8765"];
 const Delano = new Customer(...delanodata);
